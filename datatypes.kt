a01fef6fcb5566de527f225dea2d31b43604c3d4
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-09-11 02:32:10 +0530

    more on operator

--- datatypes.kt
+++ datatypes.kt
@@ -3,7 +3,7 @@ fun main(){
     var str:String="this is string";
     var num:Int=20;
     var boo:Boolean=true
-    num=20.5
+
 
     println(boo)
     println(num);
---
+++ operator.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var a=20
+    var b=30
+    var c=40
+
+    var result:Boolean;
+
+    result=a==b
+    println(result)
+
+    result=a>b
+    println(result)
+    
+
+}
